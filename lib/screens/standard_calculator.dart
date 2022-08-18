a9d32bf230d00f7832897cfe318bf5fd789e5a44
Saul <phasecross@hotmail.com>
2022-08-18 16:23:45 -0700

    Added a Drawer

--- lib/screens/standard_calculator.dart
+++ lib/screens/standard_calculator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/display.dart';
+import '../widgets/calc_drawer.dart';
 import '../widgets/keyboards/standard_keyboard.dart';
 
 class StandardCalculator extends StatelessWidget {
@@ -9,6 +10,12 @@ class StandardCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Standard'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      drawer: const CalcDrawer(),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
