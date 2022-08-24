4e6c04d50e979f18387c0f227ab8538726107657
Saul <phasecross@hotmail.com>
2022-08-24 13:25:00 -0700

    Added FontAweseome import and declared all standard buttons

--- lib/widgets/keyboards/keyboard.dart
+++ lib/widgets/keyboards/keyboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_math_fork/flutter_math.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../providers/display_provider.dart';
 
@@ -23,11 +24,137 @@ class _KeyboardState extends State<Keyboard> {
     if (!_isInit) {
       _standard = [
         KeyButton(
-          label: Provider.of<DisplayProvider>(context, listen: false)
-                  .display
-                  .isEmpty
+          label: Provider.of<DisplayProvider>(context).display.isEmpty
               ? 'C'
               : 'CE',
+          onTap: (BuildContext context) {
+            if (Provider.of<DisplayProvider>(context, listen: false)
+                .display
+                .isEmpty) {
+              Provider.of<DisplayProvider>(context, listen: false)
+                  .clearDisplay();
+            } else {
+              Provider.of<DisplayProvider>(context, listen: false).clearEntry();
+            }
+          },
+        ),
+        KeyButton(
+          icon: Icons.backspace_outlined,
+          onTap: (BuildContext context) {
+            Provider.of<DisplayProvider>(context, listen: false).backspace();
+          },
+        ),
+        const KeyButton(
+          icon: FontAwesomeIcons.percent,
+        ),
+        KeyButton(
+          icon: FontAwesomeIcons.divide,
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false).divideSymbol();
+          },
+        ),
+        KeyButton(
+          label: '7',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('7');
+          },
+        ),
+        KeyButton(
+          label: '8',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('8');
+          },
+        ),
+        KeyButton(
+          label: '9',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('9');
+          },
+        ),
+        KeyButton(
+          icon: FontAwesomeIcons.xmark,
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .multiplySymbol();
+          },
+        ),
+        KeyButton(
+          label: '4',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('4');
+          },
+        ),
+        KeyButton(
+          label: '5',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('5');
+          },
+        ),
+        KeyButton(
+          label: '6',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('6');
+          },
+        ),
+        KeyButton(
+          icon: FontAwesomeIcons.minus,
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false).minusSymbol();
+          },
+        ),
+        KeyButton(
+          label: '1',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('1');
+          },
+        ),
+        KeyButton(
+          label: '2',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('2');
+          },
+        ),
+        KeyButton(
+          label: '3',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('3');
+          },
+        ),
+        KeyButton(
+          icon: FontAwesomeIcons.plus,
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false).plusSymbol();
+          },
+        ),
+        const KeyButton(label: ''),
+        KeyButton(
+          label: '0',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('0');
+          },
+        ),
+        KeyButton(
+          label: '•',
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false)
+                .typeNumber('.');
+          },
+        ),
+        KeyButton(
+          icon: FontAwesomeIcons.equals,
+          onTap: (context) {
+            Provider.of<DisplayProvider>(context, listen: false).equals();
+          },
         ),
       ];
       _isInit = true;
@@ -36,8 +163,13 @@ class _KeyboardState extends State<Keyboard> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('xd'),
+    return GridView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+      ),
+      children: _standard,
     );
   }
 }
